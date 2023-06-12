08a4f5115161a77cfcad5b5a43c7815cb6ed59c4
unknown <a@a.com>
2023-06-12 14:25:54 +0900

    최초 플젝 생성

---
+++ src/day01/TestClass01.java
@@ -0,0 +1,15 @@
+package day01;
+
+public class TestClass01 {
+	public static void main(String[] args) {
+		//괄호 안에 내용을 출력해주는 기능
+		// ; : 쎄미콜론이며 문장의 마지막에는 꼭 있어야 한다.
+		System.out.println(100);
+		System.out.println("안녕하세요");
+		
+		System.out.print("1.안녕");
+		System.out.print("2.안녕~");
+		System.out.print("3.안녕~~");
+
+	}
+}
